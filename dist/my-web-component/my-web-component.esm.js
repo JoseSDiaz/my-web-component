fd2b29dd31292763b66f3e3e14787c25d393654d
José Solis <jose.solis@globant.com>
2024-03-13 18:32:01 -0300

    delete ..loader line

--- dist/my-web-component/my-web-component.esm.js
+++ dist/my-web-component/my-web-component.esm.js
@@ -1,16 +1,2 @@
-import { p as r, b as e } from './p-c71b9504.js';
-export { s as setNonce } from './p-c71b9504.js';
-import { g as t } from './p-e1255160.js';
-const o = () => {
-  const e = import.meta.url;
-  const s = {};
-  if (e !== '') {
-    s.resourcesUrl = new URL('.', e).href;
-  }
-  return r(s);
-};
-o().then(async r => {
-  await t();
-  return e([['p-d03eb4a4', [[1, 'my-component', { apiUrl: [1, 'api-url'], items: [32], currentIndex: [32], errorMessage: [32] }]]]], r);
-});
+import{p as r,b as e}from"./p-c71b9504.js";export{s as setNonce}from"./p-c71b9504.js";import{g as t}from"./p-e1255160.js";const o=()=>{const e=import.meta.url;const s={};if(e!==""){s.resourcesUrl=new URL(".",e).href}return r(s)};o().then((async r=>{await t();return e([["p-d03eb4a4",[[1,"my-component",{apiUrl:[1,"api-url"],items:[32],currentIndex:[32],errorMessage:[32]}]]]],r)}));
 //# sourceMappingURL=my-web-component.esm.js.map
